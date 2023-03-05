d22063ccf1f7167e3abf45532a95bdde981cec42
SJammy <samplay@one.com>
2023-03-05 19:47:17 +0000

    Update router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 
 import Home from './pages/Home'
 import Layout from './pages/Layout'
